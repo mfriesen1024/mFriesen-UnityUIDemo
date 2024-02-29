d7e1b31b12e0e656b1531da2ca7f5fb4ba9221d0
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-29 11:27:41 -0400

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,7 +13,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        FormatText();
     }
 
     // Update is called once per frame
