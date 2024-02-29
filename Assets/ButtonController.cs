9e1b5fe1ca2732c97b6290064380dcfdc52c8028
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-29 09:32:13 -0400

    Create ButtonController

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    Button b;
+    GameObject managerObj;
+    
+    [SerializeField] int value = 0;
+    [SerializeField] UpdateType type = (UpdateType)1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button b2 = b.GetComponent<Button>();
+        b2.onClick.AddListener(OnClick);
+        managerObj = GameObject.Find("UI Manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnClick(){
+    object[] sendable = {type, value};
+    managerObj.SendMessage("ScoreUpdate", sendable);
+    }
+}
