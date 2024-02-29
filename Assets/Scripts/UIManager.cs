452753b2131ccda5e3dfe1e69b494716d69afe15
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-29 10:46:43 -0400

    Cleanup code.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,3 @@
-using System;
-using TMPro;
-using UnityEngine;
-
 enum UpdateType { add, set }
 
 public class UIManager : MonoBehaviour
@@ -19,7 +15,7 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (scoreObj != null){scoreObj.text = scoreText;}
+        if (scoreObj != null) { scoreObj.text = scoreText; }
     }
 
     public void ScoreUpdate(object input)
@@ -40,33 +36,35 @@ public class UIManager : MonoBehaviour
         FormatText();
     }
 
-    void FormatText(){
-    ClampScore();
+    void FormatText()
+    {
+        ClampScore();
 
-    string tempText = score.ToString();
+        string tempText = score.ToString();
 
-    string zero = "0";
-    while(zero.Length != (9 - tempText.Length)){zero += "0";}
+        string zero = "0";
+        while (zero.Length != (9 - tempText.Length)) { zero += "0"; }
 
-    scoreText = zero + tempText;
+        scoreText = zero + tempText;
     }
 
-    void ClampScore(){score = Mathf.Clamp(score, 0, 999999999);}
+    void ClampScore() { score = Mathf.Clamp(score, 0, 999999999); }
 
     // This is a method that would handle ints instead of an object array, if I used the builtin listener.
     // However, I think that would just be really wacky and require weird workarounds, so forget that.
-    public void SampleScoreUpdate(int value){
-    // This is a local score variable so that the score is never broken by accidental use.
-    int score;
+    public void SampleScoreUpdate(int value)
+    {
+        // This is a local score variable so that the score is never broken by accidental use.
+        int score;
 
-    // Determine if value is 0 so we can set score instead of add. This is wrong.
-    if (value == 0){score = 0;} else{score += value;}
+        // Determine if value is 0 so we can set score instead of add. This is wrong.
+        if (value == 0) { score = 0; } else { score += value; }
 
-    // The flaw with the above is that we cannot set a value other than 0 without another if/else.
-    // Even then, we cannot set a nonzero value and add the same value.
-    string sampleInput = "set:14"
+        // The flaw with the above is that we cannot set a value other than 0 without another if/else.
+        // Even then, we cannot set a nonzero value and add the same value.
+        string sampleInput = "set:14"
 
     Debug.Log($"This method call works, got {value}.");
-    throw new NotImplementedException();
+        throw new NotImplementedException();
     }
 }
