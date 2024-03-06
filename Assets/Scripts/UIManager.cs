bc9b87bf6609f580b091d03bc4fb1d951d75b1bd
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-06 14:25:33 -0400

    Remove pointless code

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 enum UpdateType { add, set, random }
+enum colorChannel { r,g,b,a }
 
 public class UIManager : MonoBehaviour
 {
@@ -35,7 +36,7 @@ public class UIManager : MonoBehaviour
         if (scoreObj != null) { scoreObj.text = scoreText; }
     }
 
-    public void ScoreUpdate(object input)
+    public void ScoreUpdate(object input) // This is how I would have done it.
     {
         // Convert to enum and int
         object[] newInput = (object[])input;
@@ -110,54 +111,5 @@ public class UIManager : MonoBehaviour
         int tempScore = score;
         score = Mathf.Clamp(score, 0, 999999999);
         if (tempScore < 0) { Debug.LogError($"Attempted to set score to {tempScore}, clamp set it to {score}."); }
-    }
-
-    // This is a method that would handle ints instead of an object array, if I used the builtin listener.
-    // However, I think that would just be really wacky and require weird workarounds, so forget that.
-    public void SampleScoreUpdate(int value)
-    {
-        // This is a local score variable so that the score is never broken by accidental use.
-        int outputFromInt1 = 10; UpdateType type1;
-        int outputFromInt2 = 10; UpdateType type2; int sampleValue = 0;
-
-        // Determine if value is 0 so we can set score instead of add. This is wrong.
-        if (value == 0) { outputFromInt1 = 0; type1 = UpdateType.set; } else { outputFromInt1 += value; type1 = UpdateType.add; }
-        if (sampleValue == 0) { outputFromInt2 = 0; type2 = UpdateType.set; } else { outputFromInt2 += sampleValue; type2 = UpdateType.add; }
-
-        // The flaw with the above is that we cannot set a value other than 0 without another if/else.
-        // Even then, we cannot set a nonzero value and add the same value.
-
-        // Create some sample inputs.
-        string sampleSetInput = "set:14";
-        string sampleAddInput = "add:16";
-        // Here is another method. This is just wack because strings are weird.
-        int outputFromStringSet = ParseStringInput(sampleSetInput, out UpdateType outputSetType);
-        int outputFromStringAdd = ParseStringInput(sampleAddInput, out UpdateType outputAddType);
-
-        Debug.Log($"This method call works, got input value of {value}.");
-        Debug.Log($"Output from the int method is type {type1} with value {outputFromInt1}");
-        Debug.Log($"Output from the int method is type {type2} with value {outputFromInt2}");
-        Debug.Log($"Output from the string method with {outputSetType} mode is {outputFromStringSet}");
-        Debug.Log($"Output from the string method with {outputAddType} mode is {outputFromStringAdd}");
-        throw new NotImplementedException("This is a sample method, it doesn't do anything!");
-    }
-
-    // Here is another method. This is just wack because strings are weird. It also requires manual updating.
-    int ParseStringInput(string input, out UpdateType type)
-    {
-        //
-        int location = 1 + input.IndexOf(':');
-        int output = int.Parse(input.Substring(location));
-
-        // Get type
-        type = UpdateType.set;
-        string switchString = input.Substring(0, location - 1);
-        switch (switchString)
-        {
-            case "set": type = UpdateType.set; break;
-            case "add": type = UpdateType.add; break;
-            default: throw new NotImplementedException($"Type {switchString} is not implemented!");
-        }
-        return output;
     }
 }
